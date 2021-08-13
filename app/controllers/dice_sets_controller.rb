03319fc894ba0e56f58c0f7bcb15555f124fad02
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-13 07:37:30 -0400

    show in diceset controller

--- app/controllers/dice_sets_controller.rb
+++ app/controllers/dice_sets_controller.rb
@@ -4,4 +4,9 @@ class DiceSetsController < ApplicationController
         dice_sets = DiceSet.all
         render json: dice_sets
     end
+
+    def show 
+        dice_set = DiceSet.find(params(:id))
+        render json: dice_set
+    end
 end
