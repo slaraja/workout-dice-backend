43732375210179c78c831ff7ca30cb45913f1030
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-13 08:12:24 -0400

    workouts controller index

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,22 @@
 class WorkoutsController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    private
+
+    def workout_params
+        
+    end
+
+
 end
