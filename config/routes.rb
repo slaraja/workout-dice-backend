b776d8f63d0e93bdd49fa5e7ec9909d8a9c72d7f
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-13 16:04:20 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :workouts
-  resources :dice_sets
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :workouts, except: :update 
+  resources :dice_sets, only: :index, :show
 end
