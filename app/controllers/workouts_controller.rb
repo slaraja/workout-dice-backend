9f06b3b0fa02c877dd66f7c8360f8439721e115d
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-13 08:34:59 -0400

    workouts controller destroy

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -9,8 +9,20 @@ class WorkoutsController < ApplicationController
         workout = Workout.find(params[:id])
     end
 
+   #new action not needed because we're not rendering views
     def create
+        workout = Workout.create(workout_params)
+        if workout
+            render json: workout
+        else
+            render json: {error: 'Workout was not created.'}
+        end
+    end
 
+    def destroy
+        workout = Workout.find(params[:id])
+        workout.destroy
+        render json: {message: 'Workout was deleted.'}
     end
 
     private
