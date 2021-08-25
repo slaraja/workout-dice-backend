420551dd198d759694e9ad97da1d4a03f591a3cc
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-25 10:17:27 -0400

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,32 @@
-dice_set_a = DiceSet.create(name: "Die 1", side: "1", id: 1)
-dice_set_b = DiceSet.create(name: "Die 2", side: "1", id: 2)
+dice_set_1a = DiceSet.create(name: "Die 1", side: "1", id: 1)
+dice_set_1b = DiceSet.create(name: "Die 1", side: "2", id: 2)
+dice_set_1c = DiceSet.create(name: "Die 1", side: "3", id: 3)
+dice_set_1d = DiceSet.create(name: "Die 1", side: "4", id: 4)
+dice_set_1e = DiceSet.create(name: "Die 1", side: "5", id: 5)
+dice_set_1f = DiceSet.create(name: "Die 1", side: "6", id: 6)
 
-workout_a = Workout.create(name: "Jumping Jacks", dice_set_id: 1, id: 1)
-workout_b = Workout.create(name: "Pushups", dice_set_id: 2, id: 2)
+
+dice_set_2a = DiceSet.create(name: "Die 2", side: "1", id: 7)
+dice_set_2b = DiceSet.create(name: "Die 2", side: "2", id: 8)
+dice_set_2c = DiceSet.create(name: "Die 2", side: "3", id: 9)
+dice_set_2d = DiceSet.create(name: "Die 2", side: "4", id: 10)
+dice_set_2e = DiceSet.create(name: "Die 2", side: "5", id: 11)
+dice_set_2f = DiceSet.create(name: "Die 2", side: "6", id: 12)
+
+workout_1a = Workout.create(name: "Jumping Jacks", dice_set_id: 1, id: 1)
+workout_1b = Workout.create(name: "Push Ups", dice_set_id: 2, id: 2)
+workout_1c = Workout.create(name: "Crunches", dice_set_id: 3, id: 3)
+workout_1d = Workout.create(name: "Squats", dice_set_id: 4, id: 4)
+workout_1e = Workout.create(name: "Lunges", dice_set_id: 5, id: 6)
+workout_1f = Workout.create(name: "WILD CARD", dice_set_id: 6, id: 6)
+
+workout_2a = Workout.create(name: "10 Reps", dice_set_id: 7, id: 7)
+workout_2a = Workout.create(name: "20 Reps", dice_set_id: 8, id: 8)
+workout_2c = Workout.create(name: "30 Reps", dice_set_id: 9, id: 9)
+workout_2d = Workout.create(name: "40 Reps", dice_set_id: 10, id: 10)
+workout_2e = Workout.create(name: "50 Reps", dice_set_id: 11, id: 11)
+workout_2f = Workout.create(name: "BREAK", dice_set_id: 12, id: 12)
+
+
+
+
