0b955a6daf1e9451a60e3ab0c577222e33ae9e9d
Stephanie La Raja <stephanielaraja@gmail.com>
2021-08-26 15:34:19 -0400

    exercise controller updates

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,39 +1,39 @@
 class ExercisesController < ApplicationController
 
     def index
-        workouts = Workout.all
-        render json: workouts
+        exercises = Exercise.all
+        render json: exercises
     end
 
     def show
-        set_workout
-        render json: @workout
+        set_exercise
+        render json: @exercise
     end
 
    #new action not needed because we're not rendering views
     def create
-        workout = Workout.create(workout_params)
-        if workout
-            render json: workout
+        exercise = Exercise.create(workout_params)
+        if exercise
+            render json: exercise
         else
             render json: {error: 'Workout was not created.'}
         end
     end
 
     def destroy
-        set_workout
-        workout.destroy
+        set_exercise
+        exercise.destroy
         render json: {message: 'Workout was deleted.'}
     end
 
     private
 
-    def workout_params
-        params.require(:workout).permit(:name, :id)
+    def exercise_params
+        params.require(:exercise).permit(:name, :id)
     end
 
     def set_workout
-        @workout = Workout.find(params[:id])
+        @exercise = Exercise.find(params[:id])
     end
 
 end
